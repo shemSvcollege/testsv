470cc07293ecfdcf1a9689c65e13565d0da8829a
shem bar <shembar100@gmail.com>
2020-02-04 15:54:50 +0200

    Updates

---
+++ precache-manifest.8424c701a4f2f8b3de4c9e6e7574ff1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2234ada7244e5dad0cf6421ea4f8c23",
+    "url": "/testsv/index.html"
+  },
+  {
+    "revision": "1725b3844f6796f8c808",
+    "url": "/testsv/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "b2627f75d3355e0d8f8e",
+    "url": "/testsv/static/css/main.0ce0b383.chunk.css"
+  },
+  {
+    "revision": "1725b3844f6796f8c808",
+    "url": "/testsv/static/js/2.7cf32c17.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/testsv/static/js/2.7cf32c17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2627f75d3355e0d8f8e",
+    "url": "/testsv/static/js/main.88d0b355.chunk.js"
+  },
+  {
+    "revision": "b6ae6f51c43f814ba4ce",
+    "url": "/testsv/static/js/runtime-main.a07eca82.js"
+  }
+]);
